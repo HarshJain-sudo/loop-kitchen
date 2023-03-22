a7703ffa382557658a817d5f3a2ad71691bd05fb
hj60490 <harshj60490@gmail.com>
2023-03-22 17:35:27 +0530

    initial commit and add models

---
+++ restaurant_monitoring/models.py
@@ -0,0 +1,41 @@
+import uuid
+from django.db import models
+
+WEEKDAYS = (
+    (0, 'Monday'),
+    (1, 'Tuesday'),
+    (2, 'Wednesday'),
+    (3, 'Thursday'),
+    (4, 'Friday'),
+    (5, 'Saturday'),
+    (6, 'Sunday'),
+)
+
+
+class Store(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    timezone = models.TextField()
+
+    def __str__(self):
+        return f"{self.id}:{self.timezone}"
+
+
+class StoreStatus(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    timestamp_utc = models.DateTimeField()
+    status = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.store.name} - {self.timestamp_utc}"
+
+
+class StoreMenuHour(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    day_of_week = models.IntegerField(choices=WEEKDAYS)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+    def __str__(self):
+        return f"{self.store.name}"
