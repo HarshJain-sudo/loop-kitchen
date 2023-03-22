021801976c4ba449ad3c72cc274a4996780b9a55
hj60490 <harshj60490@gmail.com>
2023-03-22 17:55:50 +0530

    add dtos for the model

---
+++ restaurant_monitoring/dtos.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+
+@dataclass
+class StoreDTO:
+    id: str
+    timezone: str
+
+
+@dataclass
+class StoreStatusDTO:
+    id: str
+    store_id: str
+    status: str
+    timestamp_utc: datetime
+
+
+@dataclass
+class StoreMenuHourDTO:
+    id: str
+    store_id: str
+    day_of_week: int
+    start_time: datetime
+    end_time: datetime
