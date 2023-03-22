6ae83879fbd84869a833f70e67aa16e13296a885
hj60490 <harshj60490@gmail.com>
2023-03-22 17:40:05 +0530

    register models in admin and migrate

--- restaurant_monitoring/admin.py
+++ restaurant_monitoring/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Store, StoreStatus, StoreMenuHour
 
-# Register your models here.
+admin.site.register(Store)
+admin.site.register(StoreStatus)
+admin.site.register(StoreMenuHour)
