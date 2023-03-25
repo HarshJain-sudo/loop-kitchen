52190d6bceea0d08d82f517d38f55e9af7cf5702
hj60490 <harshj60490@gmail.com>
2023-03-26 00:02:15 +0530

    clean

--- restaurant_monitoring/views.py
+++ restaurant_monitoring/views.py
@@ -1,25 +1,25 @@
 from django.http import JsonResponse
-from datetime import datetime, timedelta
-from .models import StoreBusinessHour, Store
-from .utils import get_report_data
 from restaurant_monitoring.storages import storage_implementation as storage
 
 
 def trigger_report_view(request):
-    store_dtos = storage.get_all_stores()
+    store_dtos = storage\
+        .get_all_stores()
     store_ids = [
         each.store_id
         for each in store_dtos
     ]
 
-    store_menu_hour_dict = storage.get_menu_hour_dict(store_ids)
+    store_menu_hour_dict = storage.\
+        get_menu_hour_dict(store_ids)
 
     store_menu_hour_ids = [
         each['store_id']
         for each in store_menu_hour_dict
     ]
 
-    store_business_hour_dict = storage.get_business_hour_dict(store_menu_hour_ids)
+    store_business_hour_dict = storage.\
+        get_business_hour_dict(store_menu_hour_ids)
     response = {
         "store_menu_hour_dict": store_menu_hour_dict,
         "store_business_hour_dict": store_business_hour_dict
